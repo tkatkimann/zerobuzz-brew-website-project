898c70b3288c0a1c7b58c66c86a869b9b525ad94
tkatkimann <eaa23tkat@students.eaaa.dk>
2024-05-30 04:26:23 +0200

    Dropdown menu and footer have been added
    I have added a dropdown menu for "seasonal øl" and I have added social icons down in the footer.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = "flex"
-}
+    const sidebar = document.querySelector ('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector ('.sidebar')
+    sidebar.style.display = 'none'
+  }
+
+
+  function toggleDropdown(event, dropdownId) {
+    event.preventDefault();
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.classList.toggle('active');
+  }
+  
+  function showSidebar() {
+    document.querySelector('.sidebar').style.display = 'flex';
+  }
+  
+  function hideSidebar() {
+    document.querySelector('.sidebar').style.display = 'none';
+  }
+  
+  
